97363e96be0be7db736f4685f616bedebbcc6679
Aron Kuna <akuna@ruc.dk>
2024-05-23 13:20:46 +0200

    sitemap url changed

--- server/routes/sitemap.xml.js
+++ server/routes/sitemap.xml.js
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
-    hostname: 'http://localhost:3000'
+    hostname: 'https://handmade-dolls.netlify.app/'
   })
 
   for (const doc of docs) {
