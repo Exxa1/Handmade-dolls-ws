1cb3c39cf7da148f12ad5c03100509bd8f67e930
Aron Kuna <akuna@ruc.dk>
2024-05-28 23:41:37 +0200

    cookieyes added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      script: [
+        { id: 'cookieyes', src: 'https://cdn-cookieyes.com/client_data/ecdb13dd9578132dea6a023a/script.js' }
+      ],
       link: [
         {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap'}
       ]
