8ed4e3facfc3f7e30ff8271124b4088f66f4c59e
Aron Kuna <akuna@ruc.dk>
2024-05-29 14:18:13 +0200

    basic way of reviews added

--- components/content/topReviewCarousel.vue
+++ components/content/topReviewCarousel.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="not-prose">
         <ul class="flex flex-col">
-            <li v-for="review in reviews" :key="review.ID">
+            <li v-for="review in topReviews" :key="review.ID">
                 <Review>
                     <template #content>{{ review.content }}</template>
                     <template #name>{{ review.name }}</template>
@@ -14,5 +14,6 @@
 <script setup>
 
 import reviews from '~/data/reviews.json'
+const topReviews = reviews.slice(0, 3);
 
 </script>
---
+++ content/reviews.md
@@ -0,0 +1,2 @@
+::reviewCarousel
+::
